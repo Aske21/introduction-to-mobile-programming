8a1e872941d707c63b0287a34d04dea09c0a44a1
Mehovic99 <anesmehovic28@gmail.com>
2021-06-07 00:24:42 +0200

    Add Two Layouts Add Five Classes:Edited PagerAdapter Add Three Fragments: Edited Fragments Added Another Fragment: Adding Fragment

--- app/src/main/java/com/example/mobileproject/fragments/AddingFragment.java
+++ app/src/main/java/com/example/mobileproject/fragments/AddingFragment.java
@@ -7,19 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.example.mobileproject.R;
 
-import java.util.ArrayList;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link AddingFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment<listView> extends Fragment {
+public class AddingFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,7 +26,7 @@ public class HomeFragment<listView> extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public HomeFragment() {
+    public AddingFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +36,11 @@ public class HomeFragment<listView> extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment AddingFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static AddingFragment newInstance(String param1, String param2) {
+        AddingFragment fragment = new AddingFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +61,6 @@ public class HomeFragment<listView> extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_adding, container, false);
     }
-
 }
