94433cb13621d0920b9a4704788fbd7c462cd585
Mehovic99 <anesmehovic28@gmail.com>
2021-06-07 22:27:09 +0200

    Created new class for Adding recipes and its layout

---
+++ app/src/main/java/com/example/mobileproject/room/entities/AddingScreen.java
@@ -0,0 +1,16 @@
+package com.example.mobileproject.room.entities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobileproject.R;
+
+public class AddingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adding_screen);
+    }
+}
