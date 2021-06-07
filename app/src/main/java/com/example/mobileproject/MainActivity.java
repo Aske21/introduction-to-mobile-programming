d4b396fd3a5ca84d7dc444f4ce9e169b65ff91e7
Mehovic99 <anesmehovic28@gmail.com>
2021-06-07 20:56:14 +0200

    Activity

--- app/src/main/java/com/example/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_tab_fragment);
+        setContentView(R.layout.activity_login);
 
     }
 }
--- app/src/main/java/com/example/mobileproject/activities/LoginActivity.java
+++ app/src/main/java/com/example/mobileproject/activities/LoginActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 
 import com.example.mobileproject.R;
 import com.example.mobileproject.adapters.LoginAdapter;
+import com.example.mobileproject.adapters.PagerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,7 +27,31 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        TabLayout tabLayout = findViewById(R.id.TabLoginReg);
+        TabItem Home = findViewById(R.id.Login);
+        TabItem Browse = findViewById(R.id.Register);
+        ViewPager viewPager = findViewById(R.id.view_pager);
 
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
 
 
