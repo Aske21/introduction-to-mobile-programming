22224ad9a17f82da5ea4c2f1c26a654f58282469
Mehovic99 <anesmehovic28@gmail.com>
2021-06-13 20:10:57 +0200

    Added ListView Adapter

--- app/src/main/java/com/example/mobileproject/adapters/ListViewAdapter.java
+++ app/src/main/java/com/example/mobileproject/adapters/ListViewAdapter.java
@@ -1,60 +1,55 @@
 package com.example.mobileproject.adapters;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.mobileproject.Converters.ImageConverter;
 import com.example.mobileproject.R;
-import com.example.mobileproject.activities.ListViewHolder;
-import com.example.mobileproject.activities.Recipe;
+import com.example.mobileproject.room.entities.Ingredient;
+import com.example.mobileproject.room.entities.dao.IngredientDao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ListViewAdapter extends BaseAdapter {
-    public ArrayList<Recipe> listRecipes;
     private Context context;
+    private List<Ingredient> ingredientList;
 
-    public ListViewAdapter(Context context, ArrayList<Recipe> listRecipes){
+    public ListViewAdapter(Context context, List<Ingredient> ingredientList) {
         this.context = context;
-        this.listRecipes = listRecipes;
+        this.ingredientList = ingredientList;
     }
 
     @Override
     public int getCount() {
-        return listRecipes.size();
+        return ingredientList.size();
     }
 
     @Override
-    public Recipe getItem(int position) {
-        return listRecipes.get(position);
+    public Object getItem(int position) {
+        return ingredientList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return ingredientList.get(position).getRecipe_id();
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        View row;
-        final ListViewHolder listViewHolder;
-        if(convertView == null)
-        {
-            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = layoutInflater.inflate(R.layout.list_row,parent,false);
-            listViewHolder = new ListViewHolder();
-
-        }
-        return null;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        convertView = inflater.inflate(R.layout.list_row, parent, false);
+        Ingredient ingredient = ingredientList.get(position);
+        TextView itemTitle = convertView.findViewById(R.id.RecipeName);
+        TextView itemBody = convertView.findViewById(R.id.ShortDescription);
+        ImageView imageView = convertView.findViewById(R.id.imageInListView);
+        itemTitle.setText(ingredient.getTitle());
+        itemBody.setText(ingredient.getIngredients());
+        imageView.setImageBitmap(ImageConverter.convertByteArray2Image(ingredient.getImage()));
+        return convertView;
     }
 }
--- app/src/main/java/com/example/mobileproject/fragments/BrowseFragment.java
+++ app/src/main/java/com/example/mobileproject/fragments/BrowseFragment.java
@@ -7,14 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.mobileproject.R;
+import com.example.mobileproject.adapters.ListViewAdapter;
 import com.example.mobileproject.room.entities.Ingredient;
 import com.example.mobileproject.room.entities.RecipeDatabase;
 
 import java.util.List;
 
 public class BrowseFragment extends Fragment {
+    private ListView listView;
 
     public BrowseFragment() {
         // Required empty public constructor
@@ -26,12 +29,17 @@ public class BrowseFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        getAllRecipes();
-        return inflater.inflate(R.layout.fragment_browse, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_browse, container, false);
+        listView = view.findViewById(R.id.ListViewBrowse);
+        return view;
 
+    }
+
+    private void setUpListAdapter(){
+        List<Ingredient> ingredients = RecipeDatabase.getInstance(getContext()).ingredientDao().getAll();
+        ListViewAdapter adapter = new ListViewAdapter(this.getContext(), ingredients);
+        listView.setAdapter(adapter);
     }
 
     private void getAllRecipes(){
